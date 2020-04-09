603c035e754a5fa4fa72bd7f138bed4dae0ba881
Joe Sweeney <joe.sweeney224@gmail.com>
2020-04-09 10:41:35 -0600

    Saving calls

---
+++ app/Models/Need.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Need extends Model {
+}
+
+module.exports = Need
--- start/routes.js
+++ start/routes.js
@@ -17,10 +17,15 @@
 const Route = use('Route')
 const VoiceResponse = require('twilio').twiml.VoiceResponse
 
+const Need = use('App/Models/Need')
+
 Route.on('/').render('welcome')
 
-Route.get('/intro', ({ request, response }) => {
-  console.log(request.input('CallSid'))
+Route.get('/intro', async ({ request, response }) => {
+  let need = new Need()
+  need.call_sid = request.input('CallSid')
+  need.phone = request.input('From')
+  await need.save()
 
   const twiml = new VoiceResponse()
   const gather = twiml.gather({
@@ -31,22 +36,37 @@ Route.get('/intro', ({ request, response }) => {
   })
 
   gather.say('Thanks for calling "I need stuff". After the beep, please state your name. Once you\'ve stated your name, press any number to continue.')
-  twiml.say('We didn\'t receive any input. Goodbye!')
+  twiml.say('We didn\'t receive any input. If you had any trouble, please hang up and call again. Goodbye!')
 
   return response.type('text/xml').send(twiml.toString())
 })
 
-Route.get('/getname', ({ request, response }) => {
-  console.log(request.input('CallSid'))
+Route.get('/getname', async ({ request, response }) => {
+  let need = await Need.query().where('call_sid', request.input('CallSid')).first()
+  need.name = request.input('SpeechResult')
+  await need.save()
 
   const twiml = new VoiceResponse()
-  // const gather = twiml.gather({
-  //   input: 'dtmf speech',
-  //   numDigits: 1,
+  const gather = twiml.gather({
+    input: 'dtmf speech',
+    numDigits: 1,
+    action: '/getdescription',
+    method: 'GET'
+  })
 
-  // })
+  gather.say('Thanks. Now, please tell us what you need, then press any number.')
+  twiml.say('We didn\'t receive any input. If you had any trouble, please hang up and call again. Goodbye!')
 
-  twiml.say('Thanks, goodbye!')
+  return response.type('text/xml').send(twiml.toString())
+})
+
+Route.get('/getdescription', async ({ request, response }) => {
+  let need = await Need.query().where('call_sid', request.input('CallSid')).first()
+  need.description = request.input('SpeechResult')
+  await need.save()
+
+  const twiml = new VoiceResponse()
+  twiml.say('Thanks, your request has been submitted. Goodbye!')
 
   return response.type('text/xml').send(twiml.toString())
 })
