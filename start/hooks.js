f22fd2d4794f6d34ed59d08fdce6278f8a74ec4c
Joe Sweeney <joe.sweeney224@gmail.com>
2020-04-12 16:25:32 -0600

    Allow site visitor to call directly from webpage

--- start/hooks.js
+++ start/hooks.js
@@ -3,6 +3,7 @@ const { hooks } = require('@adonisjs/ignitor')
 hooks.after.providersBooted(() => {
   const View = use('View')
   const Env = use('Env')
-  View.global('numberToCall', Env.get('NUMBER_TO_CALL'))
+  View.global('numberToCallDisplay', Env.get('NUMBER_TO_CALL_DISPLAY'))
+  View.global('numberToCallActual', Env.get('NUMBER_TO_CALL_ACTUAL'))
   View.global('websocketUrl', Env.get('WEBSOCKET_URL'))
 })
