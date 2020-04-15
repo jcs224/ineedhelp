04b4e69d92ae9c5fd5cef59005cfe59b568e87b0
Joe Sweeney <joe.sweeney224@gmail.com>
2020-04-15 15:03:37 -0600

    Add dates to see when people called in

--- app/Models/Need.js
+++ app/Models/Need.js
@@ -2,8 +2,18 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const { DateTime } = require('luxon')
 
 class Need extends Model {
+  static get computed() {
+    return [
+      'created_human_friendly'
+    ]
+  }
+
+  getCreatedHumanFriendly({ created_at }) {
+    return DateTime.fromSQL(created_at).toLocaleString(DateTime.DATETIME_FULL)
+  }
 }
 
 module.exports = Need
